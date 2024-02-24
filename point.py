007708baa373699e6c15a7f438fd3652a60e1949
Steve Nicholson <ssteve@mac.com>
2024-02-24 13:29:17 -0800

    Implement add operation on Point.

--- point.py
+++ point.py
@@ -29,3 +29,6 @@ class Point:
         x = float(x_text)
         y = float(y_text)
         return Point(x, y)
+
+    def __add__(self, other: Self) -> Self:
+        return Point(self.x + other.x, self.y + other.y)
